6e70a65492444d652d8860f0df50f813c81a452e
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-25 21:27:44 +0500

    Refactor Movies component to fetch and display movie data

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,21 +1,63 @@
-import React from 'react'
-import './css/movies.css'
+import React, { useEffect, useState } from 'react';
+import './css/movies.css';
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://movies-api-dh9c.vercel.app/api/movies/');
+        const data = await response.json();
+        setMovies(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const base64toImg = (base64) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = base64;
+      img.onload = () => resolve(img);
+      img.onerror = reject;
+    });
+  };
+
+  const loadImages = async () => {
+    const moviesWithImages = await Promise.all(
+      movies.map(async (movie) => {
+        const img = await base64toImg(movie.poster);
+        return { ...movie, imgSrc: img.src };
+      })
+    );
+    setMovies(moviesWithImages);
+  };
+
+  useEffect(() => {
+    loadImages();
+  }, [movies]);
+
   return (
-    <div className='movie'>
-        <h1>Movies</h1>
+    <div className="movie">
+      <h1>Movies</h1>
 
-        <div className="movie-container">
-            <div className="movie-card">
-                <img src="https://picsum.photos/200/300" alt="movie poster" />
-                <h3>Movie Title</h3>
-                <p>Author</p>
-                <p>Release Date</p>
-            </div>
-        </div>
+      <div className="movie-container">
+        {movies.map((movie) => (
+          <div className="movie-card" key={movie._id}>
+            <img src={movie.imgSrc} alt={movie.name} />
+            <h2>{movie.name}</h2>
+            <p>Year: {movie.year}</p>
+            <p>Director: {movie.producer}</p>
+            <p>Rating: {movie.rating}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Movies
+export default Movies;
