7713a20e4e74128b72869630fb60702f3712b96b
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-25 11:43:02 +0500

    Add Register component

---
+++ src/components/Register.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './css/login.css'
+import { Link } from 'react-router-dom'
+
+const Register = () => {
+  return (
+    <div>
+        <h1>Register</h1>
+        <form>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+            <label htmlFor="password">Confirm Password</label>
+            <input type="password" id="password" name="password" />
+            <button type="submit">Register</button>
+        </form>
+        <p>Already have an account?
+            <Link to='/login'> Login</Link>
+        </p>
+    </div>
+  )
+}
+
+export default Register
