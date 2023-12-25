3f95cb4d1154958d4c5fb49adc342c2dcd7c5dd4
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-25 14:25:26 +0500

    Add Movies component and routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
 import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes } from 'react-router-dom';
+import Movies from './components/Movies';
+
 function App() {
   return (
     <>
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/movies" element={<Movies />} />
         </Routes>
       </BrowserRouter>
     </>
