2f7c425db80636c8b32e22cd0e974808461d50c0
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-25 11:42:52 +0500

    Add routes for login and signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import Register from './components/Register';
+import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
