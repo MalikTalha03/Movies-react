0a1f43b3e641dcdf8251324ff626fb879b9bd114
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-26 14:47:27 +0500

    Add login functionality to Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,49 @@
 import React from 'react'
 import './css/login.css'
 import { Link } from 'react-router-dom'
+import Cookies from 'universal-cookie'
+
 
 const Login = () => {
+
+  
+const api = 'http://localhost:8086/api/auth/login'
+  async function login(event) {
+    event.preventDefault();
+    try {
+      console.log('Login')
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+      const data = { email, password };
+      const response = await fetch(api, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        console.log('Login successful');
+        const data = await response.json();
+        new Cookies().set('auth-token', data.token);
+        
+      } else {
+        console.log('Login failed');
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  }
   return (
     <div className='login'>
         <h1>Login</h1>
         <form>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
             <label htmlFor="password">Password</label>
             <input type="password" id="password" name="password" />
-            <button type="submit">Login</button>
+            <button type="submit" onClick={(e) => login(e)}>Login</button>
         </form>
         <p>Don't have an Account yet? 
             <Link to='/signup'> Sign Up</Link>
