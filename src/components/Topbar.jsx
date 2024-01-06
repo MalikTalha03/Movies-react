533a5908e24f65a699379c577ce4e89b2df573fd
Muhammad Talha Naqshbandi <94040679+MalikTalha03@users.noreply.github.com>
2024-01-06 13:57:58 +0500

    Update Topbar.jsx

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -6,29 +6,11 @@ import Popup from 'reactjs-popup'
 
 
 const Topbar = () => {
-    useEffect(() => {
-        if (document.cookie.split(';').filter((item) => item.includes('auth-token=')).length) {
-        document.getElementById('initial').style.display = 'none';
-        document.getElementById('logged').style.display = 'block';
-        } else {
-        document.getElementById('initial').style.display = 'flex';
-        document.getElementById('logged').style.display = 'none';
-        }
-    }, [])
-    function logout() {
-        document.cookie = 'auth-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-        window.location.href = '/';
-    }
   return (
     <div className='main'>
         <h1>Movies Hub</h1>
-        <div className='init' id = "initial">
-            <button onClick = {() => window.location.href = '/login'}> Login </button>
-            <button onClick = {() => window.location.href = '/signup'}> Sign Up </button>
-        </div>
         <div className="logged" id='logged'>
         <Popup trigger={<button> Add Movie</button>} position={"bottom center"}><AddMovie /></Popup>
-            <button onClick={logout}> Logout </button>
         </div>
     </div>
   )
